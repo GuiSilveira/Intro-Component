f9ac0be6995f7783cb830b24f882a7781f34e780
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-03-05 21:55:47 -0300

    Refactoring using JavaScript

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Elements
+const inputs = document.querySelectorAll("input");
+
+const errorMsgs = Array.apply(null, Array(4));
+
+const text = [
+  "First Name cannot be empty",
+  "Last Name cannot be empty",
+  "Email cannot be empty",
+  "Password cannot be empty",
+];
+
+function createEventListeners(element, phrase) {
+  element.addEventListener("blur", function () {
+    if (element.value == 0) {
+      element.removeAttribute("required");
+      element.removeAttribute("placeholder");
+      element.classList.add("error");
+      element.after(phrase);
+    } else if (element.getAttribute("type") === "email" || element.value > 0) {
+      let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      phrase.textContent = "Looks like this is not an email";
+      element.classList.add("error");
+      element.after(phrase);
+    }
+  });
+
+  element.addEventListener("focus", function () {
+    element.classList.remove("error");
+    phrase.remove();
+  });
+}
+
+for (let i = 0; i < errorMsgs.length; i++) {
+  errorMsgs[i] = document.createElement("p");
+  errorMsgs[i].textContent = text[i];
+  errorMsgs[i].classList.add("error-msg");
+
+  createEventListeners(inputs[i], errorMsgs[i]);
+}

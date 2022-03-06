71998444e96c1dff4c3817755a98242c12cb1aba
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-03-05 22:02:16 -0300

    Ajustando erro

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createEventListeners(element, phrase) {
       element.removeAttribute("placeholder");
       element.classList.add("error");
       element.after(phrase);
-    } else if (element.getAttribute("type") === "email" && element.value > 0) {
+    } else if (element.getAttribute("type") === "email" || element.value > 0) {
       let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
       phrase.textContent = "Looks like this is not an email";
       element.classList.add("error");
